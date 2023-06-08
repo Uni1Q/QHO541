93059136ba4185c160ffb91cefb02e07433238b2
rokas <rokasjodka1@gmail.com>
2023-06-08 14:08:42 +0100

    Adjusted final JS files

--- public/app.js
+++ public/app.js
@@ -1,153 +1,271 @@
-const MongoClient = require('mongodb').MongoClient;
-const axios = require('axios');
+function deleteMovie(id){
 
-const uri = 'mongodb://127.0.0.1:27017/Netflix';
-const collectionName = 'netflixcontents';
-
-// Function to get a random item of a specific type from the database
-async function getRandomItemByType(type) {
-  try {
-    const client = await MongoClient.connect(uri);
-    const db = client.db();
-
-    // Find items of the specified type
-    const items = await db.collection(collectionName).find({ type }).toArray();
-
-    // Choose a random item from the filtered items
-    const randomItem = items[Math.floor(Math.random() * items.length)];
-
-    client.close();
-
-    return randomItem;
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
-
-// Function to display the item details in the console
-function displayItemDetails(item) {
-  console.log('Title:', item.title);
-  console.log('Type:', item.type);
-  console.log('Genre:', item.listed_in);
-  console.log('Release Year:', item.release_year);
-}
-
-// Function to fetch a random item from the API and display its details
-async function fetchRandomItem() {
-  try {
-    // Fetch a random item from the API
-    const response = await axios.get('mongodb://127.0.0.1:27017/Netflix');
-    const items = response.data;
-
-    // Choose a random item from the fetched items
-    const randomItem = items[Math.floor(Math.random() * items.length)];
-
-    // Display the item details
-    displayItemDetails(randomItem);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
+  axios.delete(`/content/${id}`, {})
+  .then(response => {
+    // Reload the page
 
 
-
-
-// Function to dynamically create HTML elements for item details
-function createItemElement(item, containerId) {
-    const container = document.getElementById(containerId);
-  
-    const title = document.createElement('div');
-    title.classList.add('movie-title');
-    title.textContent = item.title;
-    container.appendChild(title);
-  
-    const description = document.createElement('div');
-    description.textContent = item.description;
-    container.appendChild(description);
-  
-    // Add more properties as needed
-  }
-  
-  // Function to display the item details on the website
-  ///function displayItemOnWebsite(item, containerId) {
-  //  const container = document.getElementById(containerId);
-  //  container.innerHTML = ''; // Clear previous content
-  //  createItemElement(item, containerId);
- // }
-  
-  // Fetch a random TV show from the API and display it on the website
-  function fetchRandomTVShow() {
-    axios.get('mongodb://127.0.0.1:27017/Netflix')
-      .then(response => {
-        const tvShows = response.data;
-        const randomTVShow = tvShows[Math.floor(Math.random() * tvShows.length)];
-        displayItemOnWebsite(randomTVShow, 'tvShow');
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
-  
-  // Fetch a random movie from the API and display it on the website
-  function fetchRandomMovie() {
-    axios.get('mongodb://127.0.0.1:27017/Netflix')
-      .then(response => {
-        const movies = response.data;
-        const randomMovie = movies[Math.floor(Math.random() * movies.length)];
-        displayItemOnWebsite(randomMovie, 'movie');
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
-  
-
-
-
-  
-  // Call the functions to fetch and display a random TV show and movie
-  fetchRandomTVShow();
-  fetchRandomMovie();
-  
-
-// Call the functions to fetch and display a random TV show and movie
-const tvShowPromise = getRandomItemByType('TV Show');
-const moviePromise = getRandomItemByType('Movie');
-
-Promise.all([tvShowPromise, moviePromise])
-  .then(([tvShow, movie]) => {
-    if (tvShow) {
-      console.log('TV Show:');
-      displayItemDetails(tvShow);
-    } else {
-      console.log('Fetching a random TV show:');
-      fetchRandomItem();
-    }
-
-    if (movie) {
-      // If a random movie is found in the database, display its details
-      console.log('Movie:');
-      displayItemDetails(movie);
-    } else {
-      // If no random movie is found in the database, fetch a random movie from the API
-      console.log('Fetching a random movie:');
-      fetchRandomItem();
-    }
+    console.log('Delete request successful:', response.data);
+    window.location.reload();
   })
   .catch(error => {
-    console.error('Error:', error);
+    console.error('Error occurred during delete request:', error);
+    alert("Error occurred during delete request")
   });
-
-// Display data to the HTML page
-
-function displayToFrontend() {
-    axios.get('mongodb://127.0.0.1:27017/Netflix')
-        .then(response => {
-            const tvShows = response.data;
-            let divCards = document.querySelector("div.card-list")
-            divCards.innerHTML = tvShows.title
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+  
 }
+
+function onEdit(id){
+
+  window.location.href=`/edit/${id}`
+
+
+
+}
+
+function saveEdit(id) {
+  const description = document.getElementById("description").value;
+  const type = document.getElementById("type").value;
+  const title = document.getElementById("title").value;
+  const cast = document.getElementById("cast").value;
+  const country = document.getElementById("country").value;
+  const date_added = document.getElementById("date_added").value;
+  const release_year = document.getElementById("release_year").value;
+  const rating = document.getElementById("rating").value;
+  const duration = document.getElementById("duration").value;
+  const listed_in = document.getElementById("listed_in").value;
+
+  const data = {
+    description,
+    type,
+    title,
+    cast,
+    country,
+    date_added,
+    release_year,
+    rating,
+    duration,
+    listed_in
+  };
+
+  axios.put(`/content/${id}`, data)
+    .then(response => {
+      // Handle the response data
+      console.log('Response:', response.data);
+      alert("Edited Successfully");
+      window.location.href = "/";
+    })
+    .catch(error => {
+      // Handle any errors that occurred during the request
+      console.error('Error:', error);
+      alert("Unable to Edit. Please try again later!");
+    });
+}
+
+
+function handleSearch(){
+
+ searchText=document.getElementById("search").value
+  window.location.href=`/?searchText=${searchText}`
+
+
+
+}
+
+
+function saveAdd() {
+  const title = document.getElementById("title").value;
+  const type = document.getElementById("type").value;
+  const description = document.getElementById("description").value;
+  const cast = document.getElementById("cast").value;
+  const country = document.getElementById("country").value;
+  const date_added = document.getElementById("date_added").value;
+  const release_year = document.getElementById("release_year").value;
+  const rating = document.getElementById("rating").value;
+  const duration = document.getElementById("duration").value;
+  const listed_in = document.getElementById("listed_in").value;
+
+  // Perform field validation
+  if (!title || !type || !description) {
+    alert("Please fill in all required fields.");
+    return;
+  }
+
+  const data = {
+    title,
+    type,
+    description,
+    cast,
+    country,
+    date_added,
+    release_year,
+    rating,
+    duration,
+    listed_in
+  };
+
+  axios.post('/addMovie', data)
+    .then(response => {
+      // Handle the response data
+      console.log('Response:', response.data);
+      alert("Added Successfully");
+      window.location.href = "/";
+    })
+    .catch(error => {
+      // Handle any errors that occurred during the request
+      console.error('Error:', error);
+      alert("Unable to Add. Please try again later!");
+    });
+}
+
+
+
+// // Function to get a random item of a specific type from the database
+// async function getRandomItemByType(type) {
+//   try {
+//     const client = await MongoClient.connect(uri);
+//     const db = client.db();
+
+//     // Find items of the specified type
+//     const items = await db.collection(collectionName).find({ type }).toArray();
+
+//     // Choose a random item from the filtered items
+//     const randomItem = items[Math.floor(Math.random() * items.length)];
+
+//     client.close();
+
+//     return randomItem;
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// }
+
+// // Function to display the item details in the console
+// function displayItemDetails(item) {
+//   console.log('Title:', item.title);
+//   console.log('Type:', item.type);
+//   console.log('Genre:', item.listed_in);
+//   console.log('Release Year:', item.release_year);
+// }
+
+// // Function to fetch a random item from the API and display its details
+// async function fetchRandomItem() {
+//   try {
+//     // Fetch a random item from the API
+//     const response = await axios.get('mongodb://127.0.0.1:27017/Netflix');
+//     const items = response.data;
+
+//     // Choose a random item from the fetched items
+//     const randomItem = items[Math.floor(Math.random() * items.length)];
+
+//     // Display the item details
+//     displayItemDetails(randomItem);
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// }
+
+
+
+
+// // Function to dynamically create HTML elements for item details
+// function createItemElement(item, containerId) {
+//     const container = document.getElementById(containerId);
+  
+//     const title = document.createElement('div');
+//     title.classList.add('movie-title');
+//     title.textContent = item.title;
+//     container.appendChild(title);
+  
+//     const description = document.createElement('div');
+//     description.textContent = item.description;
+//     container.appendChild(description);
+  
+//     // Add more properties as needed
+//   }
+  
+//   // Function to display the item details on the website
+//   ///function displayItemOnWebsite(item, containerId) {
+//   //  const container = document.getElementById(containerId);
+//   //  container.innerHTML = ''; // Clear previous content
+//   //  createItemElement(item, containerId);
+//  // }
+  
+//   // Fetch a random TV show from the API and display it on the website
+//   function fetchRandomTVShow() {
+//     axios.get('mongodb://127.0.0.1:27017/Netflix')
+//       .then(response => {
+//         const tvShows = response.data;
+//         const randomTVShow = tvShows[Math.floor(Math.random() * tvShows.length)];
+//         displayItemOnWebsite(randomTVShow, 'tvShow');
+//       })
+//       .catch(error => {
+//         console.error('Error:', error);
+//       });
+//   }
+  
+//   // Fetch a random movie from the API and display it on the website
+//   function fetchRandomMovie() {
+//     axios.get('mongodb://127.0.0.1:27017/Netflix')
+//       .then(response => {
+//         const movies = response.data;
+//         const randomMovie = movies[Math.floor(Math.random() * movies.length)];
+//         displayItemOnWebsite(randomMovie, 'movie');
+//       })
+//       .catch(error => {
+//         console.error('Error:', error);
+//       });
+//   }
+  
+
+
+
+  
+//   // Call the functions to fetch and display a random TV show and movie
+//   fetchRandomTVShow();
+//   fetchRandomMovie();
+  
+
+// // Call the functions to fetch and display a random TV show and movie
+// const tvShowPromise = getRandomItemByType('TV Show');
+// const moviePromise = getRandomItemByType('Movie');
+
+// Promise.all([tvShowPromise, moviePromise])
+//   .then(([tvShow, movie]) => {
+//     if (tvShow) {
+//       console.log('TV Show:');
+//       displayItemDetails(tvShow);
+//     } else {
+//       console.log('Fetching a random TV show:');
+//       fetchRandomItem();
+//     }
+
+//     if (movie) {
+//       // If a random movie is found in the database, display its details
+//       console.log('Movie:');
+//       displayItemDetails(movie);
+//     } else {
+//       // If no random movie is found in the database, fetch a random movie from the API
+//       console.log('Fetching a random movie:');
+//       fetchRandomItem();
+//     }
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
+
+// // Display data to the HTML page
+
+// function displayToFrontend() {
+//     axios.get('mongodb://127.0.0.1:27017/Netflix')
+//         .then(response => {
+//             const tvShows = response.data;
+//             let divCards = document.querySelector("div.card-list")
+//             divCards.innerHTML = tvShows.title
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//         });
+// }
+
+
--- server.js
+++ server.js
@@ -1,95 +1,155 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const path =require("path")
+const path = require("path")
 const netflixContent = require('./models/netflixModels.js')
 const app = express()
 const bodyParser = require("body-parser")
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname + '/public')));
 
 // default route
 //app.get('/', function(req, res) {
-   // res.send('Netflix Content API')
+// res.send('Netflix Content API')
 //})
 
 // routes
 
 app.set("view engine", "ejs")
 
-app.get("/", async(req, res) => {
+app.get("/", async (req, res) => {
 
-    // res.send()
-    let movies = await netflixContent.aggregate({type: "Movie"}).sample(5)
-    let tvShows = await netflixContent.aggregate({type: "TV Show"}).sample(5)
-    res.render("index", {movies, tvShows})
+   const { searchText}=req.query
+
+   if(searchText){
+    let movies = await netflixContent.find({
+        type: 'Movie',
+        title: {
+          $regex: `${searchText}`,
+          $options: 'i' // 'i' option makes the search case-insensitive
+        }
+      }).limit(5);
+      ; 
+    let tvShows = await netflixContent.find({
+        type: "TV Show",
+        title: {
+          $regex: `${searchText}`,
+          $options: 'i' // 'i' option makes the search case-insensitive
+        }
+      }).limit(5);
+
+    res.render("index", { movies, tvShows,searchText })
+
+    
+   }
+   else{
+    let movies = await netflixContent.aggregate([
+        { $match: { type: "Movie" } },
+        { $limit: 5 }
+    ]);
+    let tvShows = await netflixContent.aggregate([
+        { $match: { type: "TV Show" } },
+        { $limit: 5 }
+    ])
+
+    res.render("index", { movies, tvShows,searchText:"" })
+}
 })
 
+app.get("/edit/:id", async (req, res) => {
+
+
+    let result = await netflixContent.findById(req.params.id)
+
+    if (result)
+        res.render("edit", { data: result ,searchText:""})
+    else
+        res.render("notfound", {})
+})
+
+app.get("/add", async (req, res) => {
+
+
+   
+        res.render("add",{searchText:""})
+   
+})
+
+
+
+
+
+
+
+
+
+
+
 // displays entire database
-app.get('/content', async(req, res) =>{
+app.get('/content', async (req, res) => {
     try {
         const contents = await netflixContent.find({});
         res.status(200).json(contents);
-    } catch(error){
-        res.status(500).json({message: error.message})
+    } catch (error) {
+        res.status(500).json({ message: error.message })
     }
 })
 
 // displays specific item based on ID
-app.get('/content/:id', async(req, res) =>{
+app.get('/content/:id', async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         const content = await netflixContent.findById(id);
         res.status(200).json(content);
-    } catch(error){
-        res.status(500).json({message: error.message})
+    } catch (error) {
+        res.status(500).json({ message: error.message })
     }
 })
 
 // add data
 
-app.post('/show', async(req, res) => {
-    try  {
-        const show = await netflixContent.create(req.body)
+app.post('/addMovie', async (req, res) => {
+    try {
+        const show = await netflixContent.create({...req.body,show_id:`$${Math.floor(Math.random() * (10000 - 1000 + 1)) + 1000}`})
         res.status(200).json(show);
 
-    } catch(error){
+    } catch (error) {
         console.log(error.message);
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 // update
 
-app.put('/content/:id', async(req, res)=>{
-    try{
-        const {id} = req.params;
+app.put('/content/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
         const show = await netflixContent.findByIdAndUpdate(id, req.body);
-        if(!show){
-            return res.status(404).json({message: 'cannot find any content with specified id'})
+        if (!show) {
+            return res.status(404).json({ message: 'cannot find any content with specified id' })
         }
         const updatedShow = await netflixContent.findById(id);
         res.status(200).json(updatedShow);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 // delete operation
 
-app.delete('/content/:id', async(req, res) =>{  
-    try{
-        const {id} = req.params;
+app.delete('/content/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
         const show = await netflixContent.findByIdAndDelete(id);
-        if(!show){
-            return res.status(404).json({message: 'cannot find any product with specified ID'})
+        if (!show) {
+            return res.status(404).json({ message: 'cannot find any product with specified ID' })
         }
         res.status(200).json(show);
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
@@ -97,11 +157,11 @@ app.delete('/content/:id', async(req, res) =>{
 
 mongoose.set("strictQuery", false)
 mongoose.
-connect('mongodb://127.0.0.1:27017/Netflix')
-.then(() => {
-    app.listen(3000, ()=> {
-        console.log('node API app is running on port 3000 ')
+    connect('mongodb://127.0.0.1:27017/Netflix')
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('node API app is running on port 3000 ')
+        })
+    }).catch((error) => {
+        console.log('Error')
     })
-}).catch((error) => {
-    console.log('Error')
-})
